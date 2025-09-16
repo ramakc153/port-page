67737fbb07b0b11627ba55d4a3a7cd8bd1b98252
Rama Wandya <ramawandia153@gmail.com>
2025-09-17 00:21:41 +0700

    feat (main): add log to http server

--- main.go
+++ main.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	log.Println("server served on :80")
-	http.ListenAndServe(":80", nil)
+	log.Println(http.ListenAndServe(":80", nil))
 }
